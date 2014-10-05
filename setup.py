1bcf5b60c6528f923c8e1bff202156ab96a5b119
Bill Engels <wengels@uoregon.edu>
2014-10-05 18:37:50 -0500

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,13 @@
 from setuptools import setup, find_packages
-import io
-import codecs
 import os
-import sys
 
 import ccsnmultivar
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
-long_description = read('README.rst')
-
+# translate README.md to something pypi understands
+import os
+long_description = ''
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
 
 setup(
     name = 'ccsnmultivar',
